7b8aba5b20f1d6bf38ca716e4c16f0467c063457
conrad <conrad.heidebrecht@gmail.com>
2018-05-03 17:15:18 -0400

    finally got core sorting working

--- lib/utils/activity_sorters.dart
+++ lib/utils/activity_sorters.dart
@@ -2,20 +2,8 @@ import 'package:watoplan/data/models.dart';
 
 List<T> quicksort<T>(List<T> arr, int left, int right, SortCmp<T> cmp) {
   int idx = partition<T>(arr, left, right, cmp);
-  if (left < idx - 1) quicksort(arr, left, idx-1, cmp);
+  if (left < idx - 1) quicksort(arr, left, idx - 1, cmp);
   if (right > idx) quicksort(arr, idx, right, cmp);
-  // return arr;
-  // if (arr.length <= 1) return arr;
-  
-  // T pivot = arr[0];
-  // List<T> left = <T>[];
-  // List<T> right = <T>[];
-  
-  // for (T a in arr) {
-  //   cmp(a, pivot) ? left.add(a) : right.add(a);
-  // }
-
-  // return new List<T>.from(quicksort(left, cmp))..add(pivot)..addAll(quicksort(right, cmp));
 }
 
 int partition<T>(List<T> arr, int left, int right, SortCmp<T> cmp) {
@@ -23,8 +11,8 @@ int partition<T>(List<T> arr, int left, int right, SortCmp<T> cmp) {
   int i = left;
   int j = right;
   while (i <= j) {
-    while (cmp(arr[i], pivot)) i++;
-    while (!cmp(arr[j], pivot)) j--;
+    while (cmp(arr[i], pivot, true)) i++;
+    while (cmp(arr[j], pivot, false)) j--;
     if (i <= j) {
       T tmp = arr[i];
       arr[i] = arr[j];
@@ -37,30 +25,36 @@ int partition<T>(List<T> arr, int left, int right, SortCmp<T> cmp) {
 }
 
 typedef List<Activity> ActivitySort(List<Activity> activities);
-typedef bool SortCmp<T>(T a, T b);
+typedef bool SortCmp<T>(T a, T b, bool dir);
 
 class ActivitySorters {
 
   static List<Activity> byStartTime(List<Activity> activities) {
     List<Activity> newActivities = new List.from(activities);
-    var arr = [5,1,2,8,2,];
-    quicksort(arr, 0, arr.length-1, (a, b) => a < b);
-    print(arr);
 
-    return activities;
-    // return quicksort(
-    //   newActivities,
-    //   (a, b) => a.data['start'].millisecondsSinceEpoch < b.data['start'].millisecondsSinceEpoch
-    // );
+    quicksort(
+      newActivities,
+      0, newActivities.length - 1,
+      (a, b, dir) => dir
+        ? a.data['start'].millisecondsSinceEpoch > b.data['start'].millisecondsSinceEpoch
+        : a.data['start'].millisecondsSinceEpoch < b.data['start'].millisecondsSinceEpoch
+    );
+
+    return newActivities;
   }
 
   static List<Activity> byEndTime(List<Activity> activities) {
     List<Activity> newActivities = new List.from(activities);
 
-    // return quicksort(
-    //   newActivities,
-    //   (Activity a, Activity b) => a.data['end'].millisecondsSinceEpoch < b.data['end'].millisecondsSinceEpoch
-    // );
+    quicksort(
+      newActivities,
+      0, newActivities.length - 1,
+      (a, b, dir) => dir
+        ? a.data['end'].millisecondsSinceEpoch > b.data['end'].millisecondsSinceEpoch
+        : a.data['end'].millisecondsSinceEpoch < b.data['end'].millisecondsSinceEpoch
+    );
+
+    return newActivities;
   }
 
   static List<Activity> byPriority(List<Activity> activities) {
